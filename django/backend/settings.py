b3c931d97c44c7d5d6a07ceebe7cb2b6527bee4e
aboulares <>
2025-02-26 16:42:13 +0100

    fixed database host in django app settings.py

--- django/backend/settings.py
+++ django/backend/settings.py
@@ -130,7 +130,7 @@ DATABASES = {
         'NAME':     'mydatabase',
         'USER':     'codewithmuh',
         'PASSWORD': 'admin123',
-        'HOST':     'production.cncik8g0a25i.us-west-1.rds.amazonaws.com',
+        'HOST':     'production.c5m0kyy480kr.eu-north-1.rds.amazonaws.com',
         'PORT':     '5432',
     }
 }
